d8f4f725366b7973877deea1de3caf80b01a112d
Rodrigo Melo <upperapps@gmail.com>
2023-12-16 23:22:05 -0700

    Included basic Jest configuration

---
+++ babel.config.cts
@@ -0,0 +1,5 @@
+const config = {
+  presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript']
+};
+
+export default config;
---
+++ src/services/UserService.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-warning-comments
+// TODO: Created only for Jest configuration purposes. Remove it later.
+export function add(x: number, y: number): number {
+  return x + y;
+}
+
+export function multiply(x: number, y: number): number {
+  return x * y;
+}
